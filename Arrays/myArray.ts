9d85ae3ddc6f97c2f262c4aaae2bd4e7c45e4f98
unknown <Administrator@Awt-Christina.NEPAL.ACCESSWORLD.NET>
2023-05-12 15:34:41 +0545

    array practice

--- Arrays/myArray.ts
+++ Arrays/myArray.ts
@@ -1,3 +1,24 @@
-const superHeros = []
+const superHeros: string[] = []
+// const superVillians: number[] = []
+const superVillians: Array<number> = []
 
-// superHeros.push("spiderman")
+type User = {
+    name: string;
+    isActive: boolean;
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+
+superHeros.push("spiderman")
+superVillians.push(2)
+
+allUsers.push({
+    name: "",
+    isActive: true
+})
