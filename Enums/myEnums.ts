34abde1691ca1f29cd147e76475ba882709c3067
unknown <Administrator@Awt-Christina.NEPAL.ACCESSWORLD.NET>
2023-05-14 10:58:17 +0545

    enums

---
+++ Enums/myEnums.ts
@@ -0,0 +1,16 @@
+// enum SeatChoice {
+//     AISLE = "aisle",
+//     MIDDLE = 1,
+//     WINDOW
+// }
+
+// const scSeat = SeatChoice.AISLE
+
+//Produces Less JS Code
+const enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = 1,
+    WINDOW
+}
+
+const scSeat = SeatChoice.AISLE
